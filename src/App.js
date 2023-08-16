5aadf943635cb7a95920dfbbf503d73e6efd8228
Adeshina Adenuga <Nuga0909@gmail.com>
2023-08-16 15:23:37 +0100

    add onclick event to button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Person ={ fullName,bio, imgSrc, profession} and a boolean shows.
+class App extends Component {
+  state = {
+    fullname: "Adenuga Adeshina",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, recusandae!",
+    imgSrc: "https://avatars.githubusercontent.com/u/98366229?v=4",
+    profession: "Web Developer",
+    shows: false
+  };
+
+  handleShows = () => this.setState({
+    shows: !this.state.shows
+  });
+
+
+
+  render() {
+    let showcard;
+    (this.state.shows) ? showcard = { display: 'none' } : showcard = { display: 'block' };
+
+    return (
+
+      <>
+        <div className="shows"></div>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img style={showcard} variant="top" src={this.state.imgSrc} />
+          <Card.Body>
+            <Card.Title style={showcard}>{this.state.fullname}</Card.Title>
+            <Card.Text style={showcard}>
+              {this.state.bio}
+            </Card.Text>
+            <Card.Text style={showcard}>
+              {this.state.profession}
+            </Card.Text>
+            <Button variant="primary" onClick={this.handleShows}>Show Card</Button>
+          </Card.Body>
+        </Card>
+      </>
+    );
+  }
 }
+
+{/* <div style={displa}></div> */ }
 
 export default App;
