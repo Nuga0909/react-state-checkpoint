291f20fb069bb134ad3ee2915cefbecfaa82c72b
Adeshina Adenuga <Nuga0909@gmail.com>
2023-08-16 23:29:28 +0100

    update state

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   };
 
   handleShows = () =>{
-    console.log(this.time);
+    console.log(this.state.time);
     this.setState({
       shows: !this.state.shows,
     });}
